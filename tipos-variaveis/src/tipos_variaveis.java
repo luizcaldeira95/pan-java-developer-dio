1ef9977a8de40a527aebe3974a4d1fef9f0d1d9f
Luiz Caldeira <luizcaldeira1995@gmail.com>
2023-03-07 20:17:03 -0300

    Tv finalizada

---
+++ tipos-variaveis/src/tipos_variaveis.java
@@ -0,0 +1,9 @@
+public class tipos_variaveis {
+    public static void main(String[] args) throws Exception {
+        
+        int numero = 5;
+        
+        
+        System.out.println(numero);       
+    }
+}
